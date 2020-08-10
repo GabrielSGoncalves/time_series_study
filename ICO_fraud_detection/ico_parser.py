1972c373f2bb8bfb760ceed6a75a18ef243eaf88
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2020-08-10 18:34:48 -0300

    Update parser and notebook.

--- ICO_fraud_detection/ico_parser.py
+++ ICO_fraud_detection/ico_parser.py
@@ -30,36 +30,44 @@ class ICOParser:
         self.fraud_flag = fraud_flag
         self.df = df.copy()
         self.df_resample_day = df.resample('D').sum()
-        self.df_resample_hour = df.resample('H').sum()
-        self.ico_start_date = datetime.strptime(ico_start_date, '%d/%m/%Y').replace(tzinfo=pytz.UTC)
+        # self.df_resample_hour = df.resample('H').sum()
+        self.ico_start_date = datetime.strptime(
+            ico_start_date, '%d/%m/%Y'
+        ).replace(tzinfo=pytz.UTC)
         self.ico_end_date = None
-    
+
     def define_ico_start_date(self):
         change_series = self.df_resample_day['transactions'].pct_change()
         if self.ico_start_date:
-                self.ico_end_date = self.ico_start_date + timedelta(days=60)
-        else:    
+            self.ico_end_date = self.ico_start_date + timedelta(days=60)
+        else:
             for index, value in change_series.iteritems():
 
                 if value > 50:
 
                     if index - timedelta(days=5) in change_series.index:
                         self.ico_start_date = index - timedelta(days=5)
-                        self.ico_end_date = self.ico_start_date + timedelta(days=60)
+                        self.ico_end_date = self.ico_start_date + timedelta(
+                            days=60
+                        )
                     else:
                         self.ico_start_date = change_series.index.min()
-                        self.ico_end_date = self.ico_start_date + timedelta(days=60)
+                        self.ico_end_date = self.ico_start_date + timedelta(
+                            days=60
+                        )
                     print(self.ico_start_date)
                     break
 
     def filter_df_for_training_days(self):
         if self.ico_start_date:
             return self.df_resample_day.loc[
-                (self.df_resample_day.index >= self.ico_start_date )&(
-                 self.df_resample_day.index < self.ico_end_date )]
+                (self.df_resample_day.index >= self.ico_start_date)
+                & (self.df_resample_day.index < self.ico_end_date)
+            ]
         else:
             print('First define ICO start date.')
-    
+
+    """
     def filter_df_for_training_hours(self):
         if self.ico_start_date:
             return self.df_resample_hour.loc[
@@ -67,3 +75,4 @@ class ICOParser:
                  self.df_resample_hour.index < self.ico_end_date)] 
         else:
             print('First define ICO start date.')
+    """
